d0bdb34d7dbb0d47aeb16955d750fe01f275c275
Geniy1 <golibjonodinaev@gmail.com>
2024-08-11 23:30:44 +0500

    Created animation

---
+++ app/src/main/java/com/example/myanimation/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myanimation
+
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myanimation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.alphaBtn.setOnClickListener(){
+            val animation=AnimationUtils.loadAnimation(this, R.anim.alpha_anim)
+            binding.imageView.startAnimation(animation)
+        }
+
+
+    }
+}
